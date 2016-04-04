92a75846c0f34cfbacdea5d3eeb5e45e8036340f
Tom McCluskey <bosun.tom@gmail.com>
2016-04-03 19:00:45 -0700

    initial login/registration pages and routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', function(req, res, next) {
+  console.log('got login request');
+  res.redirect('/');
+});
+
+router.post('/register', function(req, res, next) {
+  console.log('got registration request');
+  res.redirect('/');
+});
+
 module.exports = router;
